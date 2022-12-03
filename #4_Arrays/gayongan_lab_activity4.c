1ac26e3090287a50d866ccb6882113de9b95822c
Wilfeno Gayongan <WilfrenoGayongan@gmail.com>
2022-12-03 19:25:24 +0800

    Improved

--- #4_Arrays/gayongan_lab_activity4.c
+++ #4_Arrays/gayongan_lab_activity4.c
@@ -2,6 +2,10 @@
 #include <stdlib.h>
 
 void option();
+void firstOption();
+void secondOption();
+void thirdOption();
+void exitProgram();
 double averageArray(double arr[], int arrSize);
 double arrayMax(double arr[], int arrSize);
 void arraySum(double firstArray[], double secondArray[], int arrSize);
@@ -24,19 +28,20 @@ int main () {
             scanf("%d", &tryAgain);
            
         } 
-        printf("GoodBye...");
+
         
 
     } while(tryAgain == 1);
  
-    return 0;
+
+    exitProgram();
 }
 
+int operation, 
+    arraySize;
+    
 void option(){
 
-    int operation, 
-        arraySize;
-    
     do
     {
         printf("\nOPERATIONS: \n[1] to Get the average of the array.\n[2] Get the largest element in the array.\n[3] Get the sum of the two arrays.\n[4] Exit.\n\n\nPlease choose: ");
@@ -47,111 +52,127 @@ void option(){
 
     if(operation == 1)
     {
-        printf("Enter the size of the array: ");
-        scanf("%d", &arraySize);
-        double intArray[arraySize];
-        int lastElement = arraySize - 1;
-
-        for (int i = 0; i < arraySize ; i++)
-        {
-            printf("Enter element %d: ", i + 1);
-            scanf("%lf", &intArray[i]);
-        }
-        
-        printf("The average of { ");
-       
-        for(int i = 0; i < lastElement; i++) {
-
-            printf("%0.2lf, ", intArray[i]);
-
-        }
-        printf(" %0.2lf }", intArray[lastElement]);
-
-
-        printf(" is %0.2lf. ", averageArray(intArray, arraySize));
-    
+        firstOption();
     }
 
     if(operation == 2) {
 
-        printf("Enter the size of the array: ");
-        scanf("%d", &arraySize);
-
-        double intArray[arraySize];
-        int lastElement = arraySize - 1;
-
-        for (int i = 0; i < arraySize ; i++)
-        {
-            printf("Enter element %d: ", i + 1);
-            scanf("%lf", &intArray[i]);
-        }   
-
-        printf("The largest element in { ");
-       
-        for(int i = 0; i < lastElement; i++) {
-
-            printf("%0.2lf, ", intArray[i]);
-
-        }
-        printf(" %0.2lf }", intArray[lastElement]);
-
-
-        printf(" is %0.2lf. ", arrayMax(intArray, arraySize));
-        
-
-
+        secondOption();
     }
 
     if (operation == 3)
     {
-        printf("Enter the size of the array: ");
-        scanf("%d", &arraySize);
-
-        double intArray1[arraySize], intArray2[arraySize];
-        int lastElement = arraySize - 1;
-
-        printf("\nEnter the elements of the first array..\n");
-        for (int i = 0; i < arraySize ; i++)
-        {
-            printf("Enter element %d: ", i + 1);
-            scanf("%lf", &intArray1[i]);
-        }   
-        
-        printf("\nEnter the elements of the second array..\n");
-        for (int i = 0; i < arraySize ; i++)
-        {
-            printf("Enter element %d: ", i + 1);
-            scanf("%lf", &intArray2[i]);
-        }   
-
-        printf("The sum of { ");
-       
-        for(int i = 0; i < lastElement; i++) {
-
-            printf("%0.2lf, ", intArray1[i]);
-
-        }
-        printf(" %0.2lf }", intArray1[lastElement]);
-        printf(" and { ");
-
-        for(int i = 0; i < lastElement; i++) {
-
-            printf("%0.2lf, ", intArray2[i]);
-
-        }
-        printf(" %0.2lf } is ", intArray2[lastElement]);
-        arraySum(intArray1, intArray2, arraySize);
-
+        thirdOption();
         
     }
     if (operation == 4)
     {
-        printf("GoodBye...");
-        exit(0);
+        exitProgram();
     }
     
 }
 
+void firstOption(){
+    
+    
+    printf("Enter the size of the array: ");
+    scanf("%d", &arraySize);
+    double intArray[arraySize];
+    int lastElement = arraySize - 1;
+
+    for (int i = 0; i < arraySize ; i++)
+    {
+        printf("Enter element %d: ", i + 1);
+        scanf("%lf", &intArray[i]);
+    }
+    
+    printf("The average of { ");
+    
+    for(int i = 0; i < lastElement; i++) {
+
+        printf("%0.2lf, ", intArray[i]);
+
+    }
+    printf(" %0.2lf }", intArray[lastElement]);
+
+
+    printf(" is %0.2lf. ", averageArray(intArray, arraySize));
+    
+
+}
+
+void secondOption(){
+
+
+    printf("Enter the size of the array: ");
+    scanf("%d", &arraySize);
+
+    double intArray[arraySize];
+    int lastElement = arraySize - 1;
+
+    for (int i = 0; i < arraySize ; i++)
+    {
+        printf("Enter element %d: ", i + 1);
+        scanf("%lf", &intArray[i]);
+    }   
+
+    printf("The largest element in { ");
+    
+    for(int i = 0; i < lastElement; i++) {
+
+        printf("%0.2lf, ", intArray[i]);
+
+    }
+    printf(" %0.2lf }", intArray[lastElement]);
+
+
+    printf(" is %0.2lf. ", arrayMax(intArray, arraySize));
+        
+
+
+}
+
+void thirdOption(){
+    printf("Enter the size of the array: ");
+    scanf("%d", &arraySize);
+
+    double intArray1[arraySize], intArray2[arraySize];
+    int lastElement = arraySize - 1;
+
+    printf("\nEnter the elements of the first array..\n");
+    for (int i = 0; i < arraySize ; i++)
+    {
+        printf("Enter element %d: ", i + 1);
+        scanf("%lf", &intArray1[i]);
+    }   
+    
+    printf("\nEnter the elements of the second array..\n");
+    for (int i = 0; i < arraySize ; i++)
+    {
+        printf("Enter element %d: ", i + 1);
+        scanf("%lf", &intArray2[i]);
+    }   
+
+    printf("The sum of { ");
+    
+    for(int i = 0; i < lastElement; i++) {
+
+        printf("%0.2lf, ", intArray1[i]);
+
+    }
+    printf(" %0.2lf }", intArray1[lastElement]);
+    printf(" and { ");
+
+    for(int i = 0; i < lastElement; i++) {
+
+        printf("%0.2lf, ", intArray2[i]);
+
+    }
+    printf(" %0.2lf } is ", intArray2[lastElement]);
+    arraySum(intArray1, intArray2, arraySize);
+
+
+}
 double averageArray(double arr[], int arrSize){
 
     double sum, result;
@@ -194,4 +215,11 @@ void arraySum(double firstArray[], double secondArray[], int arrSize){
         printf("%0.2lf, ", firstArray[i] + secondArray[i]);
     }
     printf("%0.2lf}", firstArray[lastElement] + secondArray[lastElement]);
+}
+
+void exitProgram(){
+    
+    printf("\nGoodBye...");
+    exit(0);
+
 }
